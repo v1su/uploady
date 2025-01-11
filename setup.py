ec0e3757af3fb2e2e957c8f6f4ce6670675a8bcd
ᴠᴀꜱᴜ <106464668+v1su@users.noreply.github.com>
2025-01-11 23:34:57 +0530

    Create setup.py

---
+++ setup.py
@@ -0,0 +1,19 @@
+from setuptools import setup
+
+setup(
+    name="uploady",  # Your desired PyPI package name
+    version="0.0.1",  # Initial version
+    author="Ankit Chaubey",
+    author_email="m.ankitchaubey@gmail.com",
+    description="Placeholder for the Uploady package",
+    long_description="This is a placeholder package initial release.",
+    long_description_content_type="text/plain",
+    url="https://github.com/ankit-chaubey/uploady",  # Replace with your GitHub URL
+    packages=["uploady"],  # Main package folder
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    python_requires=">=3.6",
+)
